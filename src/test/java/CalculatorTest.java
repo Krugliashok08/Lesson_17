fd9b78a5eab07a1b16cb1e773ba5674c2a6565c6
krugliashok08 <annakm803@gmail.com>
2024-04-16 12:10:49 +0300

    Lesson_17

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,72 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class CalculatorTest {
+    public AppiumDriver driver;
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 8 API 30");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @Test
+    public void additionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='2']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='plus']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='4']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='equals']")).click();
+        driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(6,6);
+    }
+    @Test
+    public void subtractionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='5']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='minus']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='4']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='equals']")).click();
+        driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void multiplicationTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='3']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='multiply']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='3']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='equals']")).click();
+        driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(9,9);
+    }
+    @Test
+    public void divisionTest() {
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='5']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='divide']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='1']")).click();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc='equals']")).click();
+        driver.findElement(By.className("android.widget.TextView")).getText();
+        Assert.assertEquals(5,5);
+    }
+    @AfterTest
+
+    public void tearDown() {
+
+        driver.quit();
+
+    }
+}
